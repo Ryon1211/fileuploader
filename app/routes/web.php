6b3d0847b43228caa4cf1ba4fb7565f467759168
Ryon1211 <ryon1211sco@gmail.com>
2021-11-09 15:43:12 +0900

    [update] ルートファイルの修正

--- app/routes/web.php
+++ app/routes/web.php
@@ -13,12 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
-
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__ . '/auth.php';
