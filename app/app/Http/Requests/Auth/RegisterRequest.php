e797f89847bc3d878cca1c2e62ff817f6e4fc819
Ryon1211 <ryon1211sco@gmail.com>
2021-11-15 17:26:34 +0900

    [add] userの新規登録に認証キーを追加

---
+++ app/app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use App\Rules\CreateKeyCheckRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'create_authentication' => ['required', 'string', new CreateKeyCheckRule()],
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'key' => '認証キー',
+        ];
+    }
+}
