11397f09c50daacab74d1b98f5bcd85c42f03f38
Ryon1211 <ryon1211sco@gmail.com>
2021-11-22 13:03:02 +0900

    [add] ユーザー登録完了メールの送信を追加

---
+++ app/app/Notifications/RegisteredMailNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class RegisteredMailNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $name = $notifiable->name;
+
+        return (new MailMessage)
+            ->subject('登録完了のお知らせ')
+            ->greeting("Hello,$name")
+            ->line('ご登録ありがとうございます。')
+            ->line('下記のリンクからダッシュボードにアクセスしてください。')
+            ->action('ダッシュボード', url('/'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
