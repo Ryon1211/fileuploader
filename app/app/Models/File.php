6258dfeed380a5d32cbe54b788ecb8f82d8c120f
Ryon1211 <ryon1211sco@gmail.com>
2022-01-28 10:40:14 +0900

    [update] FileとDownloadLinkのModelをリファクタリング
    scope使った実装に変更

--- app/app/Models/File.php
+++ app/app/Models/File.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Upload;
+use App\Models\Download;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -24,5 +25,25 @@ class File extends Model
     public function upload()
     {
         return $this->belongsTo(Upload::class);
+    }
+
+    public function download()
+    {
+        return $this->hasMany(Download::class);
+    }
+
+    public function scopeAuthUser($query, $userId)
+    {
+        return $query->whereHas('upload.uploadLink', function ($query) use ($userId) {
+            $query->where('user_id', $userId);
+        });
+    }
+
+    public function scopeBeforeExpired($query)
+    {
+        return $query->whereHas('upload', function ($query) {
+            $query->where('uploads.expire_date', '>=', date('Y-m-d H:i:s'))
+                ->orWhere('uploads.expire_date', null);
+        });
     }
 }
