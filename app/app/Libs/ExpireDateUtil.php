8ec36255e6c69a2c02be57cb3a7671fa094f733f
Ryon1211 <ryon1211sco@gmail.com>
2022-03-01 16:37:35 +0900

    [delete] 不要になった関数を削除

--- app/app/Libs/ExpireDateUtil.php
+++ app/app/Libs/ExpireDateUtil.php
@@ -6,22 +6,17 @@ use Carbon\Carbon;
 
 class ExpireDateUtil
 {
-    public function generateExpireDatetime(?string $date): ?string
+    public function generateExpireDatetime(?string $date): mixed
     {
         if (array_key_exists($date, \DateOptionsConstants::EXPIRE_OPTIONS)) {
-            return $date ? Carbon::now()->addDay($date) : null;
+            return $date ? (string)Carbon::now()->addDay($date) : null;
         }
 
-        return $date;
+        return false;
     }
 
     public function checkExpireDate(?string $dateTime): bool
     {
         return Carbon::now()->lte(Carbon::parse($dateTime));
-    }
-
-    public function formatShowExpireDatetime(?string $date): ?string
-    {
-        return $date ? Carbon::parse($date) : null;
     }
 }
