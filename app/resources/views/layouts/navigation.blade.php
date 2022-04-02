13d51cbf12ba9defc5f43ea9348d205d0d72f427
Ryon1211 <ryon1211sco@gmail.com>
2022-04-02 10:07:48 +0900

    [update] ナビゲーションにお気に入りの項目を追加

--- app/resources/views/layouts/navigation.blade.php
+++ app/resources/views/layouts/navigation.blade.php
@@ -21,6 +21,9 @@
                     <x-nav-link :href="route('user.list')" :active="request()->routeIs('user.list')">
                         ユーザーリスト
                     </x-nav-link>
+                    <x-nav-link :href="route('user.favorite')" :active="request()->routeIs('user.favorite')">
+                        お気に入り
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -80,6 +83,9 @@
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('user.list')" :active="request()->routeIs('user.list')">
                 ユーザーリスト
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('user.favorite')" :active="request()->routeIs('user.favorite')">
+                お気に入り
             </x-responsive-nav-link>
         </div>
 
