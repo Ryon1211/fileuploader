c9c9cd196516695bed772682f4b2d9176c1cc057
Ryon1211 <ryon1211sco@gmail.com>
2022-04-03 10:45:38 +0900

    [delete] indexメソッドの削除

--- app/app/Http/Controllers/User/FavoriteController.php
+++ app/app/Http/Controllers/User/FavoriteController.php
@@ -9,36 +9,16 @@ use Illuminate\Support\Facades\Auth;
 
 class FavoriteController extends Controller
 {
-    public function index()
-    {
-        $favorites = Favorite::where('favorites.user_id', Auth::user()->id)
-            ->leftJoin('upload_links', 'favorites.upload_link_id', '=', 'upload_links.id')
-            ->leftJoin('uploads', 'upload_links.id', '=', 'uploads.upload_link_id')
-            ->select(
-                'upload_links.id as link_id',
-                'upload_links.path',
-                'upload_links.title',
-                'upload_links.created_at',
-                'upload_links.expire_date',
-                'uploads.id as upload_id',
-                'uploads.expire_date as file_expire_date'
-            )
-            ->paginate(10);
-
-        return view('user.favorite', [
-            'favorites' => $favorites,
-        ]);
-    }
-
     public function register(Request $request)
     {
         $content = $request->getContent() ?: '';
         $json = json_decode($content, true) ?? [];
         $linkId = $json['id'] ?? [];
 
-        $favorite = Favorite::where('upload_link_id', $linkId)->first();
+        $favorite = Favorite::where('upload_link_id', $linkId)
+            ->where('user_id', Auth::user()->id)->first();
 
-        if ($favorite && $favorite->user_id === Auth::user()->id) {
+        if ($favorite) {
             $favorite->delete();
         } else {
             Favorite::create([
